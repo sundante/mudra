ab5659ff364bb6d744109a0574104535dabdf89b
Suryaprakash Singh <suryaprakash.s.singh@gmail.com>
2026-05-25 21:31:16 +0530

    26/5/25 - completed phase 4 of implementations

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,28 @@ allprojects {
     }
 }
 
+// Fix 1: isar_flutter_libs missing namespace (AGP 8+ requirement)
+subprojects {
+    plugins.withId("com.android.library") {
+        extensions.configure<com.android.build.gradle.LibraryExtension> {
+            if (namespace == null) {
+                namespace = "dev.isar.isar_flutter_libs"
+            }
+        }
+    }
+}
+
+// Fix 2: isar sets compileSdk 30, but dependencies need 33+
+subprojects {
+    afterEvaluate {
+        extensions.findByType<com.android.build.gradle.LibraryExtension>()?.apply {
+            if (compileSdk != null && compileSdk!! < 34) {
+                compileSdk = 34
+            }
+        }
+    }
+}
+
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
